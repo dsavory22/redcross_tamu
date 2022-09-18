bbf7440fb6ce73f1d6f3896a90ff26a061cdb19c
DylanCravn <dylancraven@live.com>
2022-09-17 21:49:10 -0500

    Added validations for member form

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,4 +2,14 @@ class Member < ApplicationRecord
     has_many :Attendances
     has_many :Shifts, through: :Attendances
 
+#Validations for member input fields
+=begin
+	needs first name
+	needs last name
+	does NOT need email
+	does NOT need shirt size
+	does NOT need year
+=end
+    validates :First_Name, presence: true
+    validates :Last_Name, presence: true
 end
