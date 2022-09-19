616452cd7dc74077eb7026d5b02ab4ca61de5ea4
aaron54637 <aaron5463737@gmail.com>
2022-09-19 00:56:36 -0500

    added integration tests, still error with paths
    "unable to find field" error still happening.  Will fix tomorrow

---
+++ spec/feature/integration_shifts.rb
@@ -0,0 +1,23 @@
+# location: spec/feature/integration_spec.rb
+require 'rails_helper'
+
+RSpec.describe 'Creating a shift', type: :feature do
+  scenario 'valid event id' do
+    visit new_shift_path
+    fill_in 'Event_id', with: 1
+    fill_in 'Shift_Cap', with: 2
+    click_on 'New Shift'
+    visit shift_path
+    expect(page).to have_content(1)
+  end
+
+  scenario 'valid shift cap' do
+    visit new_shift_path
+    fill_in 'Event_id', with: 1
+    fill_in 'Shift_Cap', with: 2
+    click_on 'New Shift'
+    visit shift_path
+    expect(page).to have_content(2)
+  end
+
+end
