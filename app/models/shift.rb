97de867a7f8dcc2bc1f0eb262bed6567de8f52b8
DylanCravn <dylancraven@live.com>
2022-09-26 07:37:03 +0000

    final MISC

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -19,4 +19,6 @@ New Shifts
     validates :Event_id, presence: true       
 
 	validates_time :End, on_or_after: :Start, before_message: "End time must be on or after the start time"
+
+	validates :Shift_Cap, :numericality => { :greater_than_or_equal_to => 0 }
 end
