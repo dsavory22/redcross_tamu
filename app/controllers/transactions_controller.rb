f2d219f33cbfe92d505d09d2eeb4d597ec0f33de
root <root@localhost>
2022-10-12 07:12:16 +0000

    12th night

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,9 +1,11 @@
 class TransactionsController < ApplicationController
   before_action :set_transaction, only: %i[ show edit update destroy ]
 
+
   # GET /transactions or /transactions.json
   def index
     @transactions = Transaction.all
+    @budgets = Budget.all
   end
 
   # GET /transactions/1 or /transactions/1.json
