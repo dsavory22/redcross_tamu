46f7a3a5e1fac3e1619b4a0042c7de65b42a3432
root <root@localhost>
2022-10-13 04:29:29 +0000

    Cleaned styling for oauth

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,6 @@ Rails.application.routes.draw do
   resources :shifts
   resources :attendances
   resources :members
-  root 'login#index'
+  root 'members#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
