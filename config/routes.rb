f5ecc458caffdaa1f72d636a247825d2ad4d5e14
root <root@localhost>
2022-10-13 02:01:59 +0000

    File page set up

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :shifts
   resources :attendances
   resources :members
+  resources :files
   root 'members#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
