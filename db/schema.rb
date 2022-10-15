25e900915db3b08ae3800aeb52aaf7cf29935575
aaron54637 <aaron5463737@gmail.com>
2022-10-15 14:00:51 -0500

    removed "link" from views

--- db/schema.rb
+++ db/schema.rb
@@ -121,4 +121,6 @@ ActiveRecord::Schema.define(version: 2022_10_12_040835) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
